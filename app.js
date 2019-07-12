8e880fd10b25427c48413e79baed69db71d1eb6e
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-07-12 08:36:08 +0300

    adding onload photos

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = (() =>{
 
+    const photos = document.getElementsByClassName("onLoad-photo");
     const circles = [];
     const createCircles = () => {
         const maxCircles = 25;
         const circleContainer = document.getElementById('circles-container');
 
-        for (let i = 0; i < maxCircles; i++) {
+        for (let i = 1; i < maxCircles; i++) {
             const circle = document.createElement('span');
             
             circle.className = `colorize circle${i}`;
