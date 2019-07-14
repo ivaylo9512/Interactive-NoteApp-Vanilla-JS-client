29ad2105c39533a32ea67bd0a0df82da87a57465
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-07-14 23:53:04 +0300

    animate album photos

--- js/app.js
+++ js/app.js
@@ -13,7 +13,41 @@ const app = (() =>{
             .catch(function (error) {
             })
     }
+    const animateAlbumPhotos = (currentAlbum) => {
+        for (let i = 0; i < appendedPhotos.length; i++) {
+            appendedPhotos[i].style.display = 'none';
+            if(i < currentAlbum.length){
+                setTimeout(() => {
+                    appendedPhotos[i].style.display = 'block';
+                    appendedPhotos[i].style.opacity = 1;
+                    appendedPhotos[i].id = currentAlbum[i]['id'];
+                    appendedPhotos[i].src = currentAlbum[i]['location'];
+                    appendedPhotos[i].style.width = currentAlbum[i]['width'];
+                    appendedPhotos[i].style.transform = 'rotate(' + currentAlbum[i]['rotation'] + 'deg)';
+                    appendedPhotos[i].style.left = currentAlbum[i]['left'];
+                    appendedPhotos[i].style.top = currentAlbum[i]['top'];
 
+                    let noteId = currentAlbum[i]['noteId'] + "note";
+                    if(currentAlbum[i]['noteId'] != null){
+                        appendedPhotos[i].style.display = "none";
+                    }
+
+                    if(appendedPhotos[i].parentElement.className == "user-note" && appendedPhotos[i].parentElement.id != noteId){
+                        appendedPhotosSection.appendChild(appendedPhotos[i]);
+                    }
+                    
+                    if(document.getElementById(noteId)){
+                        document.getElementById(noteId).appendChild(appendedPhotos[i]);
+                    }
+        
+                    setTimeout(() => {
+                        currentAlbum[i]['left'] = window.getComputedStyle(appendedPhotos[i]).left;
+                        currentAlbum[i]['top'] = window.getComputedStyle(appendedPhotos[i]).top;
+                    }, 40)
+                }, i * 80)
+            }
+        }
+    }
     const start = () => {
         window.scrollTo(0, document.body.scrollHeight);
         window.onbeforeunload = function () {
