1090115a6cba3dc03fda4a51504104f2f164b42d
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-07-16 23:54:46 +0300

    get month

--- js/notes.js
+++ js/notes.js
@@ -83,6 +83,99 @@ const notes = (() => {
         }
     }
 
+    const getMonth = (clickedMonth) => {
+            if (clickedMonth != '') {
+                showMonth.className = 'month';
+                showMonth.id = 'month';
+                if (chosenMonthContainer.contains(showMonth)) {
+                    chosenMonthContainer.removeChild(showMonth);
+                }
+
+                if (clickedMonth != '') {
+                    showMonth.innerHTML = clickedMonth;
+                }
+
+                cloud.src = 'resources-finale/cloud-filled.png';
+                chosenMonthContainer.appendChild(showMonth);
+
+                let daysCount;
+                switch (clickedMonth) {
+                    case 'January':
+                        monthNumber = 1;
+                        daysCount = 31;
+                        break;
+                    case 'February':
+                        monthNumber = 2;
+                        daysCount = 28;
+                        break;
+                    case 'March':
+                        monthNumber = 3;
+                        daysCount = 31;
+                        break;
+                    case 'April':
+                        monthNumber = 4;
+                        daysCount = 30;
+                        break;
+                    case 'May':
+                        monthNumber = 5;
+                        daysCount = 31;
+                        break;
+                    case 'June':
+                        monthNumber = 6;
+                        daysCount = 30;
+                        break;
+                    case 'July':
+                        monthNumber = 7;
+                        daysCount = 31;
+                        break;
+                    case 'August':
+                        monthNumber = 8;
+                        daysCount = 31;
+                        break;
+                    case 'September':
+                        monthNumber = 9;
+                        daysCount = 30;
+                        break;
+                    case 'October':
+                        monthNumber = 10;
+                        daysCount = 31;
+                        break;
+                    case 'November':
+                        monthNumber = 11;
+                        daysCount = 30;
+                        break;
+                    case 'December':
+                        monthNumber = 12;
+                        daysCount = 31;
+                        break;
+                    default:
+                }
+
+                if (showMonth.innerHTML != '' && showYear.innerHTML != '' && showDay.innerHTML != '') {
+                    let date = showDay.innerHTML + '-' + monthNumber + '-' + showYear.innerHTML
+                    remote.findNoteByDate(date, currentAlbumNumber).then(
+                        res => {
+                            userNotes = res;
+                            appendNotes();
+                        }).catch(e => {
+                    })
+                }
+
+                while (daysContainer.firstChild) {
+                    daysContainer.removeChild(daysContainer.firstChild);
+                }
+
+                for (i = 1; i <= daysCount; i++) {
+                    let a = document.createElement('a');
+                    a.innerHTML = i;
+                    daysContainer.appendChild(a);
+                }
+            }
+    }
+
+    months.forEach(month => month.addEventListener('click', getMonth(months.children[0].innerHTML)))
+
+
     return {
         setCloudsAnimated,
         showMonths,
