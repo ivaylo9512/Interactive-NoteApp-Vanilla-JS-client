7ecc555fa326f596235935f1bffce3a861730e5d
ivaylo <ivayloaleksandrov@yahoo.com>
2021-06-01 00:41:01 +0300

    booleans name + ternary operator change

--- js/colorize.js
+++ js/colorize.js
@@ -1,12 +1,12 @@
 const colorize = (() => {
-    let matched;
+    let isMatched;
 
     const colors = new Array('#E2007A', '#8bb1e5', '#E2007A', '#8bb1e5', '#41291B');
     function getRandomColor() {
         const randomNumber = Math.floor(Math.random() * 5);
         const randomColor = colors[randomNumber];
 
-        matched = currentColor == randomColor ? true : false 
+        isMatched = currentColor == randomColor
 
         return randomColor;
     }
@@ -20,46 +20,41 @@ const colorize = (() => {
 
     const playBtn = document.getElementById('play-btn'); 
     const playNav = document.getElementById('play-nav');
-    let colorMode = false;
+    let isColorMode;
     const manageListeners = (e) => {
         changeBulbImages()
 
-        if(colorMode){
-            colorMode = false;
+        isColorMode ? 
             colorizables.forEach(colorizable => {
                 colorizable.removeEventListener('mouseover', changeColor);
-            });
-
-            playNav.classList.remove('play');
-        }else{
-            colorMode = true;
+            }) :
             colorizables.forEach(colorizable => {
                 colorizable.addEventListener('mouseover', changeColor);
             });
 
-            playNav.classList.add('play');
-        }
+        isColorMode = !isColorMode;
+        playNav.classList.toggle('play');
+
     }
 
-    let changed;
+    let isChanged;
     let onloadAnimation = document.getElementById('onload-animation');
     const changeBulbImages = () => {
-        if(!changed){
-            changed = true;
+        if(!isChanged){
+            isChanged = true;
             onloadAnimation.classList.add('play');
         }
     }
 
-    let currentColor = '';
+    let currentColor;
     const setCurrentColor = (color) => {
-        if(currentColor != ''){
+        if(currentColor){
             resetNodes();
         }
         currentColor = color;
     }
 
     const resetNodes = () => {
-
         colorizables.forEach(colorizable => {
             colorizable.style.color = '';
             colorizable.style.fill = '';
@@ -84,15 +79,16 @@ const colorize = (() => {
     let amountCounted = 0;
     let score = document.getElementById('score');
     const calculate = (node) => {
-        if (node.getAttribute('color') == 'marked' && !matched) {
+        if (node.getAttribute('color') == 'marked' && !isMatched) {
             amountCounted--;
             node.setAttribute('color', 'unmarked');
-        } else if (matched && (node.getAttribute('color') == 'unmarked' || node.getAttribute('color') === null)) {
+        } else if (isMatched && (node.getAttribute('color') == 'unmarked' || node.getAttribute('color') === null)) {
             amountCounted++;
             node.setAttribute('color', 'marked');
         }
         score.textContent = amountCounted;
     }
+
     const start = () => {
         getElements();
         playBtn.addEventListener('click', manageListeners);
